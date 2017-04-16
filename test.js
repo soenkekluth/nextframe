63ad79ffb6e2b3b0b7710e5fffaf6269b93ad020
Sönke Kluth <mail@soenkekluth.com>
2017-04-16 06:31:42 +0200

    tests

--- test.js
+++ test.js
@@ -1,8 +1,22 @@
 import test from 'ava';
-import nextFrame from './index';
+import nextFrame, {delay, sequence} from './index';
 
 
 test('call next frame', async t => {
 	const value = await nextFrame('check');
 	t.is(value, 'check');
+});
+
+test('delay 1s', async t => {
+	const value = await delay(1000, 'check');
+	t.is(value, 'check');
+});
+
+
+test('sequence', async t => {
+	const values = [1,2,3,4];
+	const increment = val => ++val;
+	const result = await sequence(values, increment);
+	console.log(result);
+	t.deepEqual(result, [2,3,4,5]);
 });
