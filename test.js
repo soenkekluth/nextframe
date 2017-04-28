85fa19664009e523922ed12eff9a2cb3a2362dd5
Sönke Kluth <mail@soenkekluth.com>
2017-04-28 04:59:20 +0200

    optimize tests

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
 import test from 'ava';
-import nextFrame, { delay, sequence } from './lib';
+import nextFrame, {nextFrames, delay, sequence } from './lib';
+import now from 'performance-now';
 
-test('call next frame', async t => {
+test('call next frame with argument', async t => {
   const value = await nextFrame('check');
   t.is(value, 'check');
 });
 
 test('delay 1s', async t => {
+	const start = now();
   const value = await delay(1000, 'check');
+	const duration = now() - start;
   t.is(value, 'check');
+	t.truthy(duration >= 1000 && duration <= 1100);
 });
 
 test('sequence', async t => {
@@ -16,4 +20,19 @@ test('sequence', async t => {
   const increment = val => ++val;
   const result = await sequence(values, increment);
   t.deepEqual(result, [2, 3, 4, 5]);
+});
+
+test('nextFrames loop', async t => {
+	const p = new Promise(resolve => {
+		let i = 0;
+		const cancel = nextFrames(()=>{
+			++i;
+			if(i >= 20){
+				cancel();
+				resolve(20);
+			}
+		})
+	})
+  const result = await p;
+  t.is(result, 20);
 });
